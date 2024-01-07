c43cdc83418a232efeda90ff33bc8bdc62e7be43
Kazuma Inokuchi <ddz92478@gmail.com>
2024-01-08 02:21:08 +0900

    2nd step: TDD / once ignore 1st step test

--- core/number_converter_test.go
+++ core/number_converter_test.go
@@ -7,18 +7,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNumberConverter(t *testing.T) {
-	nc := core.NumberConverter{}
-	assert.Equal(t, nc.Convert(1), "1")
-	assert.Equal(t, nc.Convert(2), "2")
-	assert.Equal(t, nc.Convert(3), "Fizz")
-	assert.Equal(t, nc.Convert(4), "4")
-	assert.Equal(t, nc.Convert(5), "Buzz")
-	assert.Equal(t, nc.Convert(6), "Fizz")
-	assert.Equal(t, nc.Convert(10), "Buzz")
-	assert.Equal(t, nc.Convert(15), "FizzBuzz")
-	assert.Equal(t, nc.Convert(30), "FizzBuzz")
-}
+// func TestNumberConverter(t *testing.T) {
+// 	nc := core.NumberConverter{}
+// 	assert.Equal(t, nc.Convert(1), "1")
+// 	assert.Equal(t, nc.Convert(2), "2")
+// 	assert.Equal(t, nc.Convert(3), "Fizz")
+// 	assert.Equal(t, nc.Convert(4), "4")
+// 	assert.Equal(t, nc.Convert(5), "Buzz")
+// 	assert.Equal(t, nc.Convert(6), "Fizz")
+// 	assert.Equal(t, nc.Convert(10), "Buzz")
+// 	assert.Equal(t, nc.Convert(15), "FizzBuzz")
+// 	assert.Equal(t, nc.Convert(30), "FizzBuzz")
+// }
 
 func TestNumberConverterWithEmptyRule(t *testing.T) {
 	nc := core.NumberConverter{Replacers: []core.N2SReplacer{}}
